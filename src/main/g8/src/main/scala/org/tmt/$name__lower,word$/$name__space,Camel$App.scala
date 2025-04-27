3f1576dc51fb04f747b3a97391a77db240ad0aa2
Allan Brighton <Allan.Brighton@t-online.de>
2025-04-27 14:04:03 +0200

    Updating for csw-6.0.0

--- src/main/g8/src/main/scala/org/tmt/$name__lower,word$/$name__space,Camel$App.scala
+++ src/main/g8/src/main/scala/org/tmt/$name__lower,word$/$name__space,Camel$App.scala
@@ -35,14 +35,14 @@ object $name;format="space,Camel"$App extends CommandsEntryPoint {
     private def start(wiring: ServerWiring, metadata: Metadata): Unit = {
       try {
         wiring.actorRuntime.startLogging(progName, appVersion)
-        wiring.logger.debug(s"starting $appName")
+        wiring.logger.debug(s"starting \$appName")
         val (binding, _) = Await.result(wiring.start(metadata), CommonTimeouts.Wiring)
-        wiring.logger.info(s"$appName online at http://${binding.localAddress.getHostString}:${binding.localAddress.getPort}/")
+        wiring.logger.info(s"\$appName online at http://\${binding.localAddress.getHostString}:\${binding.localAddress.getPort}/")
       }
       catch {
         case NonFatal(ex) =>
           ex.printStackTrace()
-          wiring.logger.error(s"$appName crashed")
+          wiring.logger.error(s"\$appName crashed")
           exit(1)
       }
     }
